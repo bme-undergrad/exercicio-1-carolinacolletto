428c4590f83340faafc846569a20e7fabace5d82
carolinacolletto <24.carolinaddcolletto@gmail.com>
2025-09-01 17:15:58 -0300

    Update exercicio1_Carolina_Colletto

--- exercicio1.m
+++ exercicio1.m
@@ -7,8 +7,27 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = 0; % alterar
+t = zeros(imax,1);
+t(1) = x0;
+erro = zeros(imax,1);
+
+for ii = 1:imax-1
+  if ii ~= 1
+    erro(ii) = (t(ii)-t(ii-1))/t(ii) * 100;   % erro relativo em %
+    if erro(ii) < es
+      break
+    endif
+  endif
+  
+  % derivada numérica simples
+  f  = func(t(ii));
+  f_d = (func(t(ii)+0.0001) - func(t(ii)))/0.0001;
+  
+  % fórmula do Newton-Raphson
+  t(ii+1) = t(ii) - f/f_d;
+endfor
+
+t = t(ii);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
